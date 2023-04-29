342c027ef33457c1fb059ddf6b68ece065039db1
andre.leite <andre.leite@miniclip.com>
2023-04-29 16:01:04 +0100

    Fix Random

--- RNG/Random.hpp
+++ RNG/Random.hpp
@@ -21,3 +21,30 @@ private:
     static thread_local std::mt19937 _mt19937Engine;
     static thread_local std::default_random_engine _defaultEngine;
 };
+
+template<typename T>
+T Random::generate(T min, T max, EngineType engineType) {
+    static_assert(std::is_arithmetic<T>::value, "T must be a numeric type.");
+
+    using Distribution = std::conditional_t<std::is_integral<T>::value,
+        std::uniform_int_distribution<T>,
+        std::uniform_real_distribution<T>>;
+    Distribution dist{ min, max };
+
+    return generateNumber<T>(dist, engineType);
+}
+
+template<typename T, typename Distribution>
+T Random::generateNumber(Distribution& dist, EngineType engineType) {
+    switch (engineType) {
+        case EngineType::MersenneTwister: {
+            return dist(_mt19937Engine);
+        }
+        case EngineType::PCG: {
+            return dist(_defaultEngine);
+        }
+        default: {
+            throw std::runtime_error("Invalid engine type.");
+        }
+    }
+}
