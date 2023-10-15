09d3164731b3d4e7ad09a63515769d081e489048
leiapollis <andreleite98@hotmail.com>
2023-10-15 09:51:31 +0100

    Add UUID generator

--- Utilities/Utilities.hpp
+++ Utilities/Utilities.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <cstdint>
+#include <random>
+#include <ostream>
 
 constexpr unsigned long long operator "" _hash(const char* s, size_t count) {
     constexpr unsigned long long fnvOffsetBasis = 14695981039346656037ULL;
@@ -11,4 +13,39 @@ constexpr unsigned long long operator "" _hash(const char* s, size_t count) {
         hash *= fnvPrime;
     }
     return hash;
+}
+
+namespace Utilities
+{
+    class UUID {
+    public:
+        UUID() {}
+
+        inline friend std::ostream& operator<<(std::ostream& os, UUID uuid);
+
+    private:
+        inline static thread_local std::mt19937 _rng = std::mt19937(std::random_device{}());
+        static constexpr bool _includeDashes = false;
+
+        std::string generate() {
+            std::string uuid;
+            for (int i = 0; i < 4; ++i) {
+                const uint32_t random_value = _rng();
+
+                char buffer[9];
+                const int ret = snprintf(buffer, sizeof(buffer), "%08x", random_value);
+                if (ret < 0 || ret >= sizeof(buffer)) {
+                    throw std::runtime_error("snprintf error");
+                }
+                uuid += buffer;
+
+                if (_includeDashes && i != 3) uuid += '-';
+            }
+            return uuid;
+        }
+    };
+
+    std::ostream& operator<<(std::ostream& os, UUID uuid) {
+        return os << uuid.generate();
+    }
 }
