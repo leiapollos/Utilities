c30022d482052aadbee78d1df67b23d5f123b9fa
andre <andreleite98@hotmail.com>
2025-02-03 19:19:52 +0000

    [WIP/Not tested] Add memory ordering to atomic

--- Utils/atomic.h
+++ Utils/atomic.h
@@ -1,132 +1,101 @@
 #pragma once
 
 #include "utils_macros.h"
+#include "typedefs.h"
 
 #if defined(_MSC_VER)
-	extern "C" long InterlockedOr64(volatile long* ptr, long value);
-	extern "C" long InterlockedExchange64(volatile long* ptr, long value);
-	extern "C" long InterlockedCompareExchange64(volatile long* ptr, long exchange, long comparand);
-#elif defined(__GNUC__)
-// GCC
-#elif defined(__clang__)
-// Clang
+    extern "C" long long InterlockedOr64(volatile long long* ptr,
+                                           long long value);
+    extern "C" long long InterlockedExchange64(volatile long long* ptr,
+                                                 long long value);
+    extern "C" long long InterlockedCompareExchange64(volatile long long* ptr,
+                                                      long long exchange,
+                                                      long long comparand);
+    #include <intrin.h>
+#else
+    #error "unsupported compiler"
 #endif
 
 namespace utils {
-	class alignas(64) atomic_int {
-	private:
-		volatile long _val;
 
-	public:
-		UTILS_ALWAYS_INLINE explicit atomic_int(int init = 0) : _val(init) {
-		}
+    enum memory_order {
+        memory_order_relaxed,
+        memory_order_acquire,
+        memory_order_release,
+        memory_order_seq_cst
+    };
 
-		UTILS_ALWAYS_INLINE int load() {
-			int tmp;
-#if defined(_MSC_VER)
-#if defined(_M_X64)
-			tmp = static_cast<int>(
-				InterlockedOr64(
-					reinterpret_cast<volatile long*>(&_val),
-					0
-				)
-				);
-#else
-			// TODO: ARM
-#endif
-#elif defined(__GNUC__) || defined(__clang__)
-#if defined(__x86_64__)
-			__asm__ __volatile__(
-				"mov %1, %0"
-				: "=r"(tmp)
-				: "m"(_val)
-				: "memory"
-			);
-#else
-			// TODO: ARM
-#endif
-#endif
-			return tmp;
-		}
+    class alignas(64) atomic_int {
+    private:
+        volatile long long _val;
 
-		UTILS_ALWAYS_INLINE void store(int x) {
-#if defined(_MSC_VER)
-#if defined(_M_X64)
-			InterlockedExchange64(
-				reinterpret_cast<volatile long*>(&_val),
-				static_cast<long>(x)
-			);
-#else
-			// TODO: ARM
-#endif
-#elif defined(__GNUC__) || defined(__clang__)
-#if defined(__x86_64__)
-			__asm__ __volatile__(
-				"mov %0, %1"
-				:
-			: "m"(_val), "r"(x)
-				: "memory"
-				);
-#else
-			// TODO: ARM
-#endif
-#endif
-		}
+    public:
+        UTILS_ALWAYS_INLINE explicit atomic_int(int init = 0)
+            : _val(init) {}
 
-		UTILS_ALWAYS_INLINE bool compare_exchange(int& expected, int desired) {
-			bool success;
-			int old = expected;
-#if defined(_MSC_VER)
-#if defined(_M_X64)
-			long prev = InterlockedCompareExchange64(
-				reinterpret_cast<volatile long*>(&_val),
-				static_cast<long>(desired),
-				static_cast<long>(old)
-			);
-			success = (prev == static_cast<long>(old));
-			if (!success) {
-				expected = static_cast<int>(prev);
-			}
-#else
-			// TODO: ARM
+        UTILS_ALWAYS_INLINE int load(memory_order order = memory_order_seq_cst) {
+            long long tmp = 0;
+#if defined(_MSC_VER) && defined(_M_X64)
+            switch (order) {
+                case memory_order_relaxed:
+                    tmp = _val;
+                    break;
+                case memory_order_acquire:
+                    tmp = _val;
+                    _ReadBarrier();
+                    break;
+                case memory_order_seq_cst:
+                    tmp = InterlockedOr64(&_val, 0);
+                    break;
+                default:
+                    UTILS_UNREACHABLE();
+            }
 #endif
-#elif defined(__GNUC__) || defined(__clang__)
-#if defined(__x86_64__)
-			__asm__ __volatile__(
-				"lock cmpxchg %3, %1\n"
-				: "=a"(old), "=m"(_val)
-				: "a"(old), "r"(desired)
-				: "memory"
-			);
-			success = (old == expected);
-			if (!success) {
-				expected = old;
-			}
-#else
-			// TODO: ARM
-#endif
-#endif
-			return success;
-		}
+            return static_cast<int>(tmp);
+        }
 
-		UTILS_ALWAYS_INLINE int atomic_increment() {
-			int old_val;
-			int new_val;
-			do {
-				old_val = load();
-				new_val = old_val + 1;
-			} while (!compare_exchange(old_val, new_val));
-			return new_val;
-		}
+        UTILS_ALWAYS_INLINE void store(int x,
+                                         memory_order order = memory_order_seq_cst) {
+            const long long val = static_cast<long long>(x);
+#if defined(_MSC_VER) && defined(_M_X64)
+            switch (order) {
+                case memory_order_relaxed:
+                    _val = val;
+                    break;
+                case memory_order_release:
+                    _WriteBarrier();
+                    _val = val;
+                    break;
+                case memory_order_seq_cst:
+                    InterlockedExchange64(&_val, val);
+                    break;
+                default:
+                    UTILS_UNREACHABLE();
+            }
+#endif
+        }
 
-		UTILS_ALWAYS_INLINE int atomic_decrement() {
-			int old_val;
-			int new_val;
-			do {
-				old_val = load();
-				new_val = old_val - 1;
-			} while (!compare_exchange(old_val, new_val));
-			return new_val;
-		}
-	};
+        UTILS_ALWAYS_INLINE bool compare_exchange(int& expected, int desired,
+                                                    memory_order order = memory_order_seq_cst) {
+            long long old_val = static_cast<long long>(expected);
+            const long long new_val = static_cast<long long>(desired);
+            const long long prev = InterlockedCompareExchange64(&_val, new_val, old_val);
+            bool success = (prev == old_val);
+            if (!success)
+                expected = static_cast<int>(prev);
+            return success;
+        }
+
+        UTILS_ALWAYS_INLINE int atomic_increment() {
+            int old_val = load(memory_order_relaxed);
+            while (!compare_exchange(old_val, old_val + 1)) {}
+            return old_val + 1;
+        }
+
+        UTILS_ALWAYS_INLINE int atomic_decrement() {
+            int old_val = load(memory_order_relaxed);
+            while (!compare_exchange(old_val, old_val - 1)) {}
+            return old_val - 1;
+        }
+    };
 }
