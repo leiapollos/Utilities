74549b8a756cd3c9b86bdb855c2b198239173abd
André Leite <andreleite@Andres-MacBook-Pro.local>
2025-06-01 15:44:44 +0100

    [WIP] New nstl atomic
    Atomic seems good on arm64 gcc and clang, but it was done blindly for x64 msvc. Need to test it there, not even sure if it compiles.

---
+++ nstl/typedefs.h
@@ -0,0 +1,17 @@
+//
+// Created by André Leite on 31/05/2025.
+//
+
+#pragma once
+
+typedef signed char i8;
+typedef unsigned char ui8;
+
+typedef short i16;
+typedef unsigned short ui16;
+
+typedef int i32;
+typedef unsigned int ui32;
+
+typedef long long i64;
+typedef unsigned long long ui64;
