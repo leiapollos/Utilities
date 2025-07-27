2b5befd95fc766de1b983d360fef012c1ff46585
leiapollos <andreleite98@hotmail.com>
2025-07-27 23:10:35 +0100

    Add bit masking (consider doing something simpler in the future?) hate templates

--- nstl/base/base_core.hpp
+++ nstl/base/base_core.hpp
@@ -53,3 +53,155 @@ debug_break(); \
 #define LIKELY(condition) (condition)
 #define UNLIKELY(condition) (condition)
 #endif
+
+
+// ////////////////////////
+// Bit masking
+
+namespace nstl {
+    template<bool B, typename T = void>
+    struct enable_if {
+    };
+
+    template<typename T>
+    struct enable_if<true, T> {
+        using type = T;
+    };
+} // namespace nstl
+
+#define Bit(n) (1ULL << (n))
+#define BitMask(start, end) ((Bit((end) - (start) + 1) - 1) << (start))
+
+template<typename E>
+struct bitmask_enum_traits {
+    static constexpr bool enabled = false;
+    using underlying_type = void;
+};
+
+#define ENABLE_BITMASK_OPERATORS(E, T)			\
+	template <>									\
+	struct bitmask_enum_traits<E> {				\
+		static constexpr bool enabled = true;	\
+		using underlying_type = T;				\
+	};
+
+template<typename E>
+struct Flags;
+
+template<typename E>
+typename nstl::enable_if<bitmask_enum_traits<E>::enabled, Flags<E>>::type
+operator|(E lhs, E rhs) {
+    return Flags<E>(lhs) | rhs;
+}
+
+template<typename E>
+typename nstl::enable_if<bitmask_enum_traits<E>::enabled, Flags<E>>::type
+operator&(E lhs, E rhs) {
+    return Flags<E>(lhs) & rhs;
+}
+
+template<typename E>
+typename nstl::enable_if<bitmask_enum_traits<E>::enabled, Flags<E>>::type
+operator^(E lhs, E rhs) {
+    return Flags<E>(lhs) ^ rhs;
+}
+
+template<typename E>
+typename nstl::enable_if<bitmask_enum_traits<E>::enabled, Flags<E>>::type
+operator~(E val) {
+    return ~Flags<E>(val);
+}
+
+template<typename E>
+struct Flags {
+    using underlying = typename bitmask_enum_traits<E>::underlying_type;
+    underlying value;
+
+    constexpr Flags() : value(0) {
+    }
+
+    constexpr Flags(E flag) : value(static_cast<underlying>(flag)) {
+    }
+
+    constexpr explicit Flags(underlying v) : value(v) {
+    }
+
+    constexpr Flags<E>& set(Flags<E> flags, bool condition) {
+        if (condition) {
+            value |= flags.value;
+        } else {
+            value &= ~flags.value;
+        }
+        return *this;
+    }
+
+    constexpr Flags<E>& set(Flags<E> flags) {
+        value |= flags.value;
+        return *this;
+    }
+
+    constexpr Flags<E>& clear(Flags<E> flags) {
+        value &= ~flags.value;
+        return *this;
+    }
+
+    constexpr Flags<E>& toggle(Flags<E> flags) {
+        value ^= flags.value;
+        return *this;
+    }
+
+    constexpr bool has(Flags<E> flags) const {
+        return (value & flags.value) == flags.value;
+    }
+
+    constexpr bool any(Flags<E> flags) const {
+        return (value & flags.value) != 0;
+    }
+
+    constexpr Flags<E>& operator|=(Flags<E> other) {
+        value |= other.value;
+        return *this;
+    }
+
+    constexpr Flags<E>& operator&=(Flags<E> other) {
+        value &= other.value;
+        return *this;
+    }
+
+    constexpr Flags<E>& operator^=(Flags<E> other) {
+        value ^= other.value;
+        return *this;
+    }
+
+    constexpr Flags<E> operator|(Flags<E> other) const {
+        return Flags<E>(value | other.value);
+    }
+
+    constexpr Flags<E> operator&(Flags<E> other) const {
+        return Flags<E>(value & other.value);
+    }
+
+    constexpr Flags<E> operator^(Flags<E> other) const {
+        return Flags<E>(value ^ other.value);
+    }
+
+    constexpr Flags<E> operator~() const {
+        return Flags<E>(~value);
+    }
+
+    constexpr bool operator==(Flags<E> other) const {
+        return value == other.value;
+    }
+
+    constexpr bool operator!=(Flags<E> other) const {
+        return value != other.value;
+    }
+
+    constexpr explicit operator bool() const {
+        return value != 0;
+    }
+
+    constexpr explicit operator underlying() const {
+        return value;
+    }
+};
